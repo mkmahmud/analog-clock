b97e306e250d01904ce35995a6efacdf4902ccdf
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-09-18 09:00:14 +0600

    get dom element in js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Getting Dom Element
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+
+
+
